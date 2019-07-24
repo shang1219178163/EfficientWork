18e744ee0c59c3df85e96c638c40ec8b4a5faea0
Shang <shang1219178163@gmail.com>
2019-07-24 18:03:08 +0800

    update

--- build/Shells/common.sh
+++ build/Shells/common.sh
@@ -13,3 +13,31 @@ datetimeStamp(){
     echo "${result}"
 }
 
+# 在当前时间追加时间 天 时 分 秒
+# datetimeAdding(){
+#     # echo_blue $#
+#     local now=$(datetimeStamp)
+#     local tmp;  
+#     case $# in  
+#             1)
+#             tmp=($now + $1*86400);
+
+#             2)
+#             tmp=($now + $1*86400 + $2*3600);;
+    
+#             3)
+#             tmp=($now + $1*86400 + $2*3600 + $3*60);;
+
+#             4)
+#             tmp=($now + $1*86400 + $2*3600 + $3*60 + $4);;
+
+#             *)
+#             {
+#             tmp=now
+#             echo_red "在当前时间追加时间: 天 时 分 秒(4个参数)"
+#             };;
+                    
+#     esac
+#     echo "${tmp}"
+
+# }
