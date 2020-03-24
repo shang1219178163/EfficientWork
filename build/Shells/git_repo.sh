1671679c763a498e3906730f4c4c9d6fcca756cf
Shang <shang1219178163@gmail.com>
2020-03-24 12:00:22 +0800

    修复bug

--- build/Shells/git_repo.sh
+++ build/Shells/git_repo.sh
@@ -6,6 +6,34 @@ source Shells/git_action.sh
 
 export LANG="zh_CN.GB2312"
 
+
+#创建并关联到远程仓库
+#libCreateAndLinkRepo(){
+#    username=shang1219178163
+#    echo_green "--- ${username}: $1 ---"
+#
+#     echo_green "--- Step: pod lib create $1 --template-url=https://github.com/$username/pod-template.git ---"
+#     pod lib create $1 --template-url=https://github.com/$username/pod-template.git || exit 1
+#
+#    echo_green "--- Step: curl -u ${username} https://api.github.com/user/repos -d '{\"name\":\"$1\"}' ---"
+#    curl -u ${username} https://api.github.com/user/repos -d '{"name":"$1"}' || exit 1
+#
+#    echo_green "--- Step: git remote rm origin ---"
+#    git remote rm origin || exit 1
+#
+#    echo_green "--- Step: git remote add origin ssh://git@github.com:${username}/$1.git ---"
+#    git remote add origin ssh://git@github.com:${username}/$1.git
+#
+#    echo_green "--- Step: git remote set-url origin https://github.com/${username}/$1.git ---"
+#    git remote set-url origin https://github.com/${username}/$1.git || exit 1
+#
+#    echo_green "--- Step: git push --force --all ---"
+#    git push --force --all || exit 1
+#
+#    echo_yellow "--- Step: finished ！---"
+#}
+
+
 #创建本地你仓库 local lib
 createLib(){
     username=shang1219178163
@@ -14,11 +42,11 @@ createLib(){
     echo_green "--- Step: pod lib create $1 --template-url=https://github.com/$username/pod-template.git ---"
     pod lib create $1 --template-url=https://github.com/$username/pod-template.git || exit 1
 
-   libCreateAndLinkRepo $1;
+   linkRepo $1;
 }
 
-#创建并关联到远程仓库
-libCreateAndLinkRepo(){
+#关联到远程仓库
+linkRepo(){
     username=shang1219178163
     echo_green "--- ${username}: $1 ---"
 
@@ -61,9 +89,9 @@ if [ -d "$fileNameAll" ]
 then
    # echo_green "--- 存在：${fileNameAll} ---"
    echo_green "--- 发现lib: $(datetime) ---"
-   libCreateAndLinkRepo $fileNameAll;
+   linkRepo $fileNameAll;
 else
-   echo_red "--- lib不存在：$fileNameAll，创建本地lib ---"
+   echo_red "--- lib不存在：${fileNameAll}，创建本地lib ---"
    createLib $fileNameAll;
 fi
 
