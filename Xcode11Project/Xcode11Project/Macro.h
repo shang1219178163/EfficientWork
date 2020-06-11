cc9364d86b099a09d57cb7ed976f10c0cff9ec8a
Shang <shang1219178163@gmail.com>
2020-06-11 15:12:53 +0800

    宏文件

--- Xcode11Project/Xcode11Project/Macro.h
+++ Xcode11Project/Xcode11Project/Macro.h
@@ -10,4 +10,29 @@
 #define Macro_h
 
 
+#ifdef DEBUG
+#define NNLog(fmt, ...) NSLog((@"[Line %d] %s " fmt), __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+
+#else
+#define NNLog(...)
+#endif
+
+#ifdef DEBUG
+#define DDLog(FORMAT, ...) {\
+NSString *formatStr = @"yyyy-MM-dd HH:mm:ss.SSSSSSZ";\
+NSMutableDictionary *threadDic = NSThread.currentThread.threadDictionary;\
+NSDateFormatter *formatter = [threadDic objectForKey:formatStr];\
+if (!formatter) {\
+formatter = [[NSDateFormatter alloc]init];\
+formatter.dateFormat = formatStr;\
+[threadDic setObject:formatter forKey:formatStr];\
+}\
+NSString *str = [formatter stringFromDate:NSDate.date];\
+fprintf(stderr,"%s【line %d】%s %s\n",[str UTF8String], __LINE__, __PRETTY_FUNCTION__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);\
+}
+
+#else
+#define DDLog(...)
+#endif
+
 #endif /* Macro_h */
