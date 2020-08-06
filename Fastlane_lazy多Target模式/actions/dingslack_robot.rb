5483dbcf94630d6a5ee4c9f4a29c5ac376fb41d6
Shang <shang1219178163@gmail.com>
2020-08-06 19:26:14 +0800

    添加 slack 通知支持

--- Fastlane_lazy多Target模式/actions/dingslack_robot.rb
+++ Fastlane_lazy多Target模式/actions/dingslack_robot.rb
@@ -1,21 +1,22 @@
 module Fastlane
   module Actions
     module SharedValues
-      DINGDINGTALK_ROBOT_CUSTOM_VALUE = :DINGDINGTALK_ROBOT_CUSTOM_VALUE
+      DINGSLACK_ROBOT_CUSTOM_VALUE = :DINGSLACK_ROBOT_CUSTOM_VALUE
     end
 
-    class DingdingtalkRobotAction < Action
+    class DingslackRobotAction < Action
       def self.run(params)
         # fastlane will take care of reading in the parameter and fetching the environment variable:
         # UI.message "Parameter API Token: #{params[:api_token]}"
+
         # sh "shellcommand ./path"
-        # Actions.lane_context[SharedValues::DINGDINGTALK_ROBOT_CUSTOM_VALUE] = "my_val"
+
+        # Actions.lane_context[SharedValues::DINGSLACK_ROBOT_CUSTOM_VALUE] = "my_val"
         appPath = params[:appPath]
-        appIcon = params[:appIcon]
-        dingUrl = params[:dingUrl]
         downloadUrl = params[:downloadUrl]
+        appIcon = params[:appIcon]
+        slackUrl = params[:slackUrl]
         slogan = params[:slogan]
-        markdownText = params[:markdownText]
 
         appName    = other_action.get_ipa_info_plist_value(ipa: appPath, key: "CFBundleDisplayName")
         bundleName = other_action.get_ipa_info_plist_value(ipa: appPath, key: "CFBundleBundleName")
@@ -26,41 +27,39 @@ module Fastlane
         appName = appName.empty? == false ? appName : bundleName
 
         platformInfo = "已更新至 #{downloadUrl.split(".")[1].capitalize} 啦！"
-        title = "iOS #{appName} #{appVersion} #{platformInfo}"
+        message = "iOS #{appName} v#{appVersion} #{platformInfo}"
+
+        author = `git log -1 --pretty=format:"%ae"`
         time = Time.new.strftime("%Y-%m-%d %H:%M:%S")
 
-        markdown ={
-          msgtype: "link",
-          link: {
-              title: title,
-              text: "版  本：#{appBuild}\n地  址：#{downloadUrl}\n时  间：#{time}",
-              picUrl: "#{appIcon}",
-              messageUrl: "#{downloadUrl}"
+        other_action.slack(
+          # message: "<项目名称> Successfully deployed new App Update.",
+          message: message,
+          slack_url: slackUrl,
+          # default_payloads: [:lane, :git_branch, :git_author, :last_git_commit],
+          default_payloads: [],
+          attachment_properties: {
+            thumb_url: appIcon,
+            author_name: author,
+            pretext: slogan,
+            fields: [
+            {
+              # title: "DownloadUrl",
+              value: "版本：#{appBuild}",
+              short: true
+            },
+            {
+              # title: "DownloadUrl",
+              value: "下载：#{downloadUrl}",
+              short: true
+            },
+            {
+              # title: "时间日期",
+              value: "时间：#{time}",
+              short: false
+            }]
           }
-        }
-
-        if markdownText
-          markdownText = "#{markdownText}   \n  - [Download](#{downloadUrl})"
-          markdown ={
-               "msgtype": "markdown",
-               "markdown": {"title": "#{title}",
-                            "text": "### #{title}\n#{markdownText}",
-               }
-           }
-        end
-
-        ##请求
-        uri = URI.parse(dingUrl)
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = true
-
-        request = Net::HTTP::Post.new(uri.request_uri)
-        request.add_field('Content-Type', 'application/json')
-        request.body = markdown.to_json
-
-        response = https.request(request)
-        puts "-----------#{uri.request_uri}-------------------"
-        puts "Response #{response.code} #{response.message}: #{response.body}"
+        )
       end
 
       #####################################################
@@ -92,13 +91,9 @@ module Fastlane
                                   description: "ipa图标网络地址",
                                      optional: true,
                                          type: String),
-             FastlaneCore::ConfigItem.new(key: :dingUrl,
-                                  description: "钉钉机器人网络接口",
+             FastlaneCore::ConfigItem.new(key: :slackUrl,
+                                  description: "slack webhook Url",
                                      optional: false,
-                                         type: String),
-             FastlaneCore::ConfigItem.new(key: :markdownText,
-                                  description: "钉钉机器人 msgtype: markdown时的text",
-                                     optional: true,
                                          type: String),
              FastlaneCore::ConfigItem.new(key: :slogan,
                                   description: "slogan",
@@ -111,7 +106,7 @@ module Fastlane
         # Define the shared values you are going to provide
         # Example
         [
-          ['DINGDINGTALK_ROBOT_CUSTOM_VALUE', 'A description of what this value contains']
+          ['DINGSLACK_ROBOT_CUSTOM_VALUE', 'A description of what this value contains']
         ]
       end
 
