397eff58f6d964ea6e7252f62cd3ae4edae9bff0
Shang <shang1219178163@gmail.com>
2024-09-13 20:59:45 +0800

    feat：新增git提交转周报脚本

---
+++ weekly_project_out.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#项目文件外部执行此脚本
+
+project_name="*"
+
+# 获取当前脚本所在目录的路径
+script_dir=$(dirname "$(realpath "$0")")
+echo "脚本所在目录: $script_dir"
+
+
+# # 获取当前脚本的绝对路径
+# script_path=$(realpath "$0")
+# echo "当前脚本路径: $script_path"
+
+
+target_dir="$script_dir/$project_name"
+
+# 判断目录是否存在
+if [ -d "$target_dir" ]; then
+  # 进入目标目录
+  cd "$target_dir" || { echo "无法进入目录: $target_dir"; exit 1; }
+  echo "已进入目录: $(pwd)"
+else
+  echo "目录不存在: $target_dir"
+fi
+
+
+
+git_author=$(git show -s --format='%ae')
+echo "git_author: $git_author"
+
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "git_branch: $git_branch"
+
+# 获取当前日期的倒数5天的日期
+date_start=$(date -v -5d +%Y-%m-%d)
+echo "date_start: $date_start"
+
+git log --author="${git_author}" --after="$date_start" --pretty=format:"%s" > ../commit_messages.txt
